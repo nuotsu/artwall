54965e920cdaf533187e71127086051ef956bf1a
Mitchell Christ <xzmaru@gmail.com>
2023-05-29 11:52:10 -0700

    adjust grabbing

--- src/lib/Media.svelte
+++ src/lib/Media.svelte
@@ -12,6 +12,8 @@
 	<img
 		class="w-full"
 		class:grabber={editable}
+		class:grabbing-l={grabbing && grabbing < -GRABBING_THRESHOLD}
+		class:grabbing-r={grabbing && grabbing > GRABBING_THRESHOLD}
 		src={image || ''}
 		alt={title || ''}
 		draggable={false}
@@ -53,9 +55,8 @@
 		cursor: grabbing;
 	}
 
-	figure:has(.grabber:active) {
-		@apply rotate-2;
-	}
+	figure:has(.grabbing-l) { @apply rotate-2; }
+	figure:has(.grabbing-r) { @apply -rotate-2; }
 </style>
 
 <script lang="ts">
@@ -78,17 +79,25 @@
 
 	let grabber: HTMLElement
 	let moving = false
+	let grabbing: number | false = 0
+
+	const GRABBING_THRESHOLD = 0.0609 as const
 
 	function mousemove(e: MouseEvent) {
 		if (!editable || !moving) return
 
-		left += (e.movementX / window.innerWidth * 100)
-		top += (e.movementY / window.innerHeight * 100)
+		let x_inc = e.movementX / window.innerWidth * 100
+
+		left += x_inc
+		top += e.movementY / window.innerHeight * 100
+
+		grabbing = x_inc
 	}
 
 	async function mouseup(e: MouseEvent) {
 		if (!editable || (e.target as HTMLElement).closest('.grabber') !== grabber) return
 		moving = false
+		grabbing = false
 
 		await move(form)
 	}
@@ -101,5 +110,6 @@
 	function keyup(e: KeyboardEvent) {
 		if (!editable || e.key !== 'Escape') return
 		moving = false
+		grabbing = false
 	}
 </script>
